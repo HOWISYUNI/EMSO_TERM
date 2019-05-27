60266b7a9900d2976232a6a8af101740b6f43cf9
min <o_ogog@naver.com>
2019-05-27 13:40:01 +0900

    light_sensor finish.

---
+++ R1/KERNEL/light_sensor/device_lib.c
@@ -0,0 +1,45 @@
+#include "device_lib.h"
+/* LED device path : /dev/LED */
+
+/* turn on led */
+int turn_on_led(){
+    int ret;
+    ret = open("/dev/led", O_RDWR);
+    return ret;
+}
+
+/* turn off led */
+int turn_off_led(int fd){
+    int ret;
+    ret = close(fd);
+    return ret;
+}
+
+int turn_on_pir(){
+	int ret;
+	ret = open("/dev/pir", O_RDWR);
+	
+	return ret;	/* success : 0 */
+}
+
+int turn_off_pir(int fd){
+	int ret;
+	ret = close(fd);
+
+	return ret;	/* success : 0 */
+}
+
+/* read value from light sensor*/
+int read_light_sensor(){
+	int dev;
+	int ret;
+	int light_value=0;
+
+	dev = open("/dev/light_sensor_dev",O_RDWR);
+	
+	ret=read(dev,&light_value,sizeof(int));
+	
+	ret = close(dev);
+
+	return light_value;
+}
---
+++ R1/KERNEL/light_sensor/device_lib.h
@@ -0,0 +1,10 @@
+#include <sys/fcntl.h>		
+#include <unistd.h>		/* close */
+
+int turn_on_led();
+int turn_off_led(int fd);
+
+int turn_on_pir();
+int turn_off_pir(int fd);
+
+int read_light_sensor();
