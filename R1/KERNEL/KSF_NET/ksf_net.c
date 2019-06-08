004ae98a9af0c9909b64662d81dd94e2907f7989
aeomhs <t_aeom@naver.com>
2019-06-09 02:05:07 +0900

    init kernel socket

--- R1/KERNEL/KSF_NET/ksf_net.c
+++ R1/KERNEL/KSF_NET/ksf_net.c
@@ -5,26 +5,112 @@
  */
 #include "ksf_net.h"
 
+/* create a socket */
 static int ksf_net_open(struct inode *inode, struct file *file){
+    int err;    
+
+    if ( ( (err = sock_create(AF_INET, SOCK_DGRAM, IPPROTO_UDP, &sock)) < 0) ||
+         ( (err = sock_create(AF_INET, SOCK_DGRAM, IPPROTO_UDP, &sock_send)) < 0 )) {
+            printk(KERN_INFO KSF_NET": Could not create a datagram socket, error = %d\n", -ENXIO);
+            return -1;
+    }
+
+    memset(&addr, 0, sizeof(struct sockaddr));
+    memset(&addr_send, 0, sizeof(struct sockaddr));
+    addr.sin_family      = AF_INET;
+    addr_send.sin_family = AF_INET;
+
+    addr.sin_addr.s_addr      = htonl(INADDR_ANY);
+    addr_send.sin_addr.s_addr = htonl(INADDR_SEND);
+
+    addr.sin_port      = htons(DEFAULT_PORT);
+    addr_send.sin_port = htons(CONNECT_PORT);
+    
+    if ( ( (err = sock->ops->bind(sock, (struct sockaddr *)&addr, sizeof(struct sockaddr) ) ) < 0) ||
+           (err = sock_send->ops->connect(sock_send, (struct sockaddr *)&addr_send, sizeof(struct sockaddr), 0) < 0 )){
+            printk(KERN_INFO KSF_NET": Could not bind or connect to socket, error = %d\n", -err);
+            sock_release(sock);
+            sock_release(sock_send);
+            return -1;
+    }
+
+    return 0;
 }
 
 static int ksf_net_release(struct inode *inode, struct file *file){
+    sock_release(sock);
+    sock = NULL;
+
+    printk(KERN_INFO KSF_NET": socket released\n");
+    return 0;
 }
 
-static long ksf_net_ioctl(struct file *file, unsigned int cmd, unsigned long arg){
-}
 
 static int ksf_net_read(struct file *file, char *buf, size_t len, loff_t *loff){
+    struct msghdr msg;      /* Msg */
+    struct iovec iov;       /* Data */
+    int size = 0;
+    unsigned long nr_segments = 1;
+    size_t count = 1;
+
+    if (sock->sk==NULL) 
+        return 0;
+
+    /* msg set */
+    msg.msg_flags = 0;
+    msg.msg_name = &addr;            /* Socket name */
+    msg.msg_namelen  = sizeof(struct sockaddr_in);
+    msg.msg_control = NULL;         /* (void*)msg Control */
+    msg.msg_controllen = 0;
+    /* Data set */
+    iov.iov_base = buf;
+    iov.iov_len = len;
+    iov_iter_init(&msg.msg_iter, READ, &iov, nr_segments, count);
+    /* receive msg */
+    size = sock_recvmsg(sock, &msg, msg.msg_flags);
+
+    return size;
 }
 
 static int ksf_net_write(struct file *file, const char *buf, size_t len, loff_t *loff){
+    struct msghdr msg;
+    struct iovec iov;
+    int size = 0;
+    void *k_buf;
+    int ret;
+    unsigned long nr_segments = 1;
+    size_t count = 1;
+    
+    if(sock->sk == NULL)
+        return 0;
+
+    /* Msg Set */
+    msg.msg_flags = 0;
+    msg.msg_name = &addr;            /* Socket name */
+    msg.msg_namelen  = sizeof(struct sockaddr_in);
+    msg.msg_control = NULL;         /* (void*)msg Control */
+    msg.msg_controllen = 0;
+    /* Copy Data from user */
+    k_buf = (void*)kmalloc(sizeof(len), GFP_KERNEL);
+    ret = copy_from_user(k_buf, buf, len);
+    if(ret < 0)
+        return -1;
+    /* Data set */
+    iov.iov_base = k_buf;
+    iov.iov_len = len;
+    iov_iter_init(&msg.msg_iter, READ, &iov, nr_segments, count);
+    /* Send msg */
+    size = sock_sendmsg(sock, &msg);
+
+    kfree(k_buf);
+    return size;
 }
 
 struct file_operations ksf_net_fops =
 {
 	.read = ksf_net_read,
 	.write = ksf_net_write,
-	.unlocked_ioctl = ksf_net_ioctl,
+	/* .unlocked_ioctl = ksf_net_ioctl, */
 	.open = ksf_net_open,
 	.release = ksf_net_release
 };
--- R1/KERNEL/KSF_NET/ksf_net.h
+++ R1/KERNEL/KSF_NET/ksf_net.h
@@ -10,14 +10,22 @@
 #include <linux/fs.h>
 #include <linux/fcntl.h>
 #include <linux/cdev.h>
-
+#include <linux/uaccess.h>
+#include <linux/slab.h>
+/* kernel socket programming */
 #include <linux/net.h>
 #include <linux/socket.h>
+#include <linux/uio.h>
+#include <linux/ip.h>
+#include <linux/in.h>
+
 
 #define KSF_NET "ksf_net"
 
-static dev_t dev_num;
-static struct cdev *cd_cdev;
+#define DEFAULT_PORT 2325   /* Src */
+#define CONNECT_PORT 23     /* Dest */
+//#define INADDR_SEND ((unsigned long int)0x7f000001) /* 127.0.0.1 */
+#define INADDR_SEND INADDR_LOOPBACK
 
 /*
  * @GET | type           | data      | cmd           
@@ -57,30 +65,27 @@ struct response{
     void *data;
 };
 
-struct 
+static dev_t dev_num;
+static struct cdev *cd_cdev;
+/* src */
+static struct socket *sock;
+static struct sockaddr_in addr;
+/* dest */
+static struct socket *sock_send;
+static struct sockaddr_in addr_send;
 
-/* Socket init
- * return socket
- */
+/* create socket */
 static int ksf_net_open(struct inode *inode, struct file *file);
 
-/* Socket close
- * return result
- */
+/* release socket */
 static int ksf_net_release(struct inode *inode, struct file *file);
 
-/* ICOTL 1 : Server Init
- * IOCTL 2 : Client Init
- */
-static long ksf_net_ioctl(struct file *file, unsigned int cmd, unsigned long arg);
+/*  */
+//static long ksf_net_ioctl(struct file *file, unsigned int cmd, unsigned long arg);
 
-/* Send data
- * return result
- */
+/* Send data */
 static int ksf_net_read(struct file *file, char *buf, size_t len, loff_t *loff);
 
-/* receive data
- * return result
- */
+/* receive data */
 static int ksf_net_write(struct file *file, const char *buf, size_t len, loff_t *loff);
 
