8d965d7edbb68e67fd8bdb8f75e7310cb01c0671
aeomhs <t_aeom@naver.com>
2019-06-11 22:46:43 +0900

    clean ksf_net

--- R3/lib/KSF_NET/ksf_server_lib.h
+++
@@ -1,6 +0,0 @@
-#include "ksf_net.h"
-
-int server_open(int port);
-int server_close(int sock);
-int wait_request(int sock, struct request *req);
-int response(int c_sock, char type, unsigned long len, char *data);
