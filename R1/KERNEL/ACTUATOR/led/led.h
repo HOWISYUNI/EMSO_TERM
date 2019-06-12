c421880f535bb71e2fba93c15299ad0be64421fc
aeomhs <t_aeom@naver.com>
2019-06-13 01:08:05 +0900

    timer led

--- R1/KERNEL/ACTUATOR/led/led.h
+++ R1/KERNEL/ACTUATOR/led/led.h
@@ -4,20 +4,27 @@
 #include <linux/fs.h>
 #include <linux/gpio.h>
 #include <linux/cdev.h>
+#include <linux/timer.h>
+
+#define IOCTL_START_NUM 0x80
+#define IOCTL_NUM1 IOCTL_START_NUM+1
+#define IOCTL_NUM2 IOCTL_START_NUM+2
+#define IOCTL_NUM3 IOCTL_START_NUM+3
+
+#define LED_IOCTL_NUM 'z'
+#define TURN_ON_LED _IOWR(LED_IOCTL_NUM, IOCTL_NUM1, unsigned long *)
+#define TURN_OFF_LED _IOWR(LED_IOCTL_NUM, IOCTL_NUM2, unsigned long *)
+#define TIME_LED _IOWR(LED_IOCTL_NUM, IOCTL_NUM3, unsigned long *)
 
 #define DEV_LED "led"
 #define LED 4
 
 /* 
- * function : LED on
- * parameter : file path, mode
- * return : file descriptor
+ * function : ioctl LED
+ * parameter : Command ( TURN_ON_LED | TURN_OFF_LED | TIME_LED )
+ * return : SUCCESS(0)
+ * @TURN_ON_LED : Turn on led
+ * @TURN_OFF_LED : Turn off led
+ * @TIME_LED : while "arg" second, turn on led
  * */
-static int led_open(struct inode *inode, struct file *file);
-/*
- * function : LED off
- * parameter : file path, mode
- * return : file descriptor
- * */
-static int led_release(struct inode *inode, struct file *file);
-
+static long led_ioctl(struct file *file, unsigned int cmd, unsigned long arg);
