a6492b5319f40d1ff91d9f3c5796c1d051e1dbc7
aeomhs <t_aeom@naver.com>
2019-06-15 01:20:29 +0900

    pir kernel & lib

--- R1/KERNEL/SENSOR/pir/pir.c
+++ R1/KERNEL/SENSOR/pir/pir.c
@@ -2,34 +2,32 @@
 
 MODULE_LICENSE("GPL");
 
-static int a = 0;
+static int detect = FALSE;
 static int irq_num;
 wait_queue_head_t wq;
 
 static long pir_ioctl(struct file *file, unsigned int cmd, unsigned long arg){
-	enable_irq(irq_num);
 
-	if(cmd == (0x80+1)){
-		wait_event(wq, a != 0);
-
-		a = 0;
-		disable_irq(irq_num);
-
-		return 0;
-	}
-
-	return -1;
+    switch(cmd){
+        case DETECT_WAIT:
+            enable_irq(irq_num);
+            detect = FALSE;
+            wait_event(wq, detect == FALSE); /* wake up when detected from isr */
+            disable_irq(irq_num);
+            return 0;
+            
+        default :
+            return -1;
+    }
 }
 
 struct file_operations pir_fops =
 {
-	.open = pir_open,
-	.release = pir_release,
 	.unlocked_ioctl = pir_ioctl,
 };
 
 static irqreturn_t pir_isr(int irq, void* dev_id){
-	a = 1;
+	detect = TRUE;
 	wake_up(&wq);
 
 	return IRQ_HANDLED;
@@ -41,18 +39,18 @@ static struct cdev *cd_cdev;
 static int __init pir_init(void){
 	int ret;
 
-	printk("Init PIR Module!!\n");
-
-	alloc_chrdev_region(&dev_num, 0, 1, DEV_NAME);
+    /* cdev init */
+	alloc_chrdev_region(&dev_num, 0, 1, DEV_PIR);
 	cd_cdev = cdev_alloc();
 	cdev_init(cd_cdev, &pir_fops);
 	cdev_add(cd_cdev, dev_num, 1);
 
+    /* gpio & interrupt init */
 	gpio_request_one(SENSOR, GPIOF_IN, "pir");
 	irq_num = gpio_to_irq(SENSOR);
-	ret = request_irq(irq_num, pir_isr, IRQF_TRIGGER_RISING | IRQF_TRIGGER_FALLING, "pir_irq", NULL);
+	ret = request_irq(irq_num, pir_isr, IRQF_TRIGGER_RISING, "pir_irq", NULL);
 	
-	/* fail */
+	/* irq fail */
 	if(ret){
 		printk(KERN_ERR "Unable to request IRQ: %d\n", ret);
 		free_irq(irq_num, NULL);
@@ -67,7 +65,6 @@ static int __init pir_init(void){
 }
 
 static void __exit pir_exit(void){
-	printk("Exit PIR Module!!\n");
 	cdev_del(cd_cdev);
 	unregister_chrdev_region(dev_num, 1);
 
