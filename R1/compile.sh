3803c3dfda5449cf010e3afe8e8678a92b0ad538
aeomhs <t_aeom@naver.com>
2019-06-14 21:56:59 +0900

    net_lib testing

--- R1/compile.sh
+++ R1/compile.sh
@@ -1,18 +1,16 @@
 #mkdir ./bin
+rm -r bin
 mkdir bin
 
 #lib compile
-#arm-linux-gnueabi-gcc -c ./lib/ACTUATOR/actuator_lib.c
-arm-linux-gnueabi-gcc -c ./lib/KSF_NET/ksf_server_lib.c
-arm-linux-gnueabi-gcc -c ./lib/KSF_NET/ksf_client_lib.c
+arm-linux-gnueabi-gcc -c ./lib/KSF_NET/ksf_net_lib.c
 
 #app compile
-#arm-linux-gnueabi-gcc -c ./App/sf_subctr.c
-#arm-linux-gnueabi-gcc -I. -o daemon ./App/daemon.c actuator_lib.o sf_subctr.o
-arm-linux-gnueabi-gcc -I. -o server ./App/ksf_net_server.c ksf_server_lib.o
-arm-linux-gnueabi-gcc -I. -o client ./App/ksf_net_client.c ksf_client_lib.o
+arm-linux-gnueabi-gcc -I. -o server ./App/examples/ksf_net_server.c ksf_net_lib.o
+arm-linux-gnueabi-gcc -I. -o client ./App/examples/ksf_net_client.c ksf_net_lib.o
 
 #delete .o
-#rm actuator_lib.o sf_subctr.o
-rm ksf_server_lib.o ksf_client_lib.o
+rm ksf_net_lib.o
 
+scp server client pi@10.10.10.15:~/emso/TP
+rm server client
