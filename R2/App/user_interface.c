b09b0adefc1fded525ee2e4aab4fd39d446a75e8
konis123 <konis123@naver.com>
2019-06-15 15:35:09 +0900

    유저인터페이스 추가 및 수정 커맨드컨트롤러, 데이터컨트롤러 예외처리, 스프링클러 수정

--- R2/App/user_interface.c
+++ R2/App/user_interface.c
@@ -4,35 +4,67 @@ int main(){
 	
 	int input;
 
-	struct request rcv;
-
-	//int socket_r1 = client_open(R1_ADDR, R1_ACT_PORT);
-	//int socket_r3 = client_open(R3_ADDR, R3_REF_PORT);
-	int socket_r3 = client_open("127.0.0.1", 4000);
-
-
-	int soil = 0;
-	int light = 0;
-
+	char rcv_str[BUF];
+	int soil_a;
+	int light_a;
+	
+	char c;
+	//char ss[1024] = "aaaa\nbbbb\ncccc\ndddd\n";
+	//char c;
+	//parse(ss);
+	//scanf("%c", &c);
 	while(1){
 		system("clear");
 		printf("-----------------------------------------\n");
-		printf("* 1. r1의 스프링클러 켜기                    *\n");
-		printf("* 2. r1의 led 켜기                           *\n");
-		printf("* 3. r3에 토양온습도 평균값 요청        *\n");
-		printf("* 4. r3에 조도 평균값 요청              *\n");
+		printf("* 1. r1의 스프링클러 켜기               *\n");
+		printf("* 2. r1의 led 켜기                      *\n");
+		printf("* 3. r3에 토양온습도 최근 n개 요청      *\n");
+		printf("* 4. r3에 조도 최근 n개 요청            *\n");
+		printf("* 5. r3에 토양온습도 평균값 요청        *\n");
+		printf("* 6. r3에 조도 평균값 요청              *\n");
+		printf("* 0. 종료                               *\n");
 		printf("-----------------------------------------\n");
-		printf("토양온습도 평균값:%d  조도 평균값:%d     \n\n", soil, light);
+		printf("토양온습도 평균값:%d  조도 평균값:%d     \n\n", soil_a, light_a);
 		scanf("%d", &input);
 
 		if(input == 1){
-			
+			int socket_r1 = client_open(R1_ADDR, R1_ACT_PORT, 5);
+
+			client_close(socket_r1);			
 		}else if(input == 2){
-			
+			int socket_r1 = client_open(R1_ADDR, R1_ACT_PORT, 5);
+
+			client_close(socket_r1);
 		}else if(input == 3){
-			soil = request_humidity_to_r3(socket_r3);
+			int socket_r3 = client_open(R3_ADDR, R3_REF_PORT, 5);
+			request_soil_recent_to_r3(socket_r3, rcv_str);
+			client_close(socket_r3);
+			printf("--최근 토양온습도 값들--\n");
+			parse(rcv_str);
+			scanf("%c", &c);
 		}else if(input == 4){
-			light = request_light_to_r3(socket_r3);
+			int socket_r3 = client_open(R3_ADDR, R3_REF_PORT, 5);
+			request_light_recent_to_r3(socket_r3, rcv_str);
+			client_close(socket_r3);
+			printf("--최근 조도 값들--\n");
+			parse(rcv_str);
+			scanf("%c", &c);
+		}else if(input == 5){
+			int socket_r3 = client_open(R3_ADDR, R3_REF_PORT, 5);
+			soil_a = request_soil_average_to_r3(socket_r3);
+			client_close(socket_r3);
+			printf("토양온습도 평균값: %d\n", soil_a);
+			scanf("%c", &c);
+		}else if(input == 6){
+			int socket_r3 = client_open(R3_ADDR, R3_REF_PORT, 5);
+			light_a = request_light_average_to_r3(socket_r3);
+			client_close(socket_r3);
+			printf("조도 평균값: %d\n", light_a);
+			scanf("%c", &c);
+		}else if(input == 0){
+			printf("종료합니다.\n");
+			scanf("%c", &c);
+			break;
 		}else{
 
 		}
@@ -44,21 +76,52 @@ int main(){
 }
 
 /*3번*/
-int request_humidity_to_r3(int socket){
+void request_soil_recent_to_r3(int socket, char* str){
+	struct response rcv;
+	char rcv_data[BUF];
+	rcv = request(socket, 'G', 's', 'r', 1, "5");
+	//rcv_data = rcv.data;
+	strcpy(str, rcv.data);
+}
+
+/*4번*/
+void request_light_recent_to_r3(int socket, char* str){
+	struct response rcv;
+	char rcv_data[BUF];
+	rcv = request(socket, 'G', 'l', 'r', 1, "5");
+	strcpy(str, rcv.data);
+
+}
+
+/*5번*/
+int request_soil_average_to_r3(int socket){
 	struct response rcv;
 	int rcv_data;
-	rcv = request(socket, 'G', 's', 'a', 0, "");
+	rcv = request(socket, 'G', 's', 'a', 2, "10");
 	rcv_data = (int)rcv.data;
 	
 	return rcv_data;
 }
 
-/*4번*/
-int request_light_to_r3(int socket){
+/*6번*/
+int request_light_average_to_r3(int socket){
 	struct response rcv;
 	int rcv_data;
-	rcv = request(socket, 'G', 'l', 'a', 0, "");
+	rcv = request(socket, 'G', 'l', 'a', 2, "10");
 	rcv_data = (int)rcv.data;	
 
 	return 0;
+}
+
+/*recent값으로 여러개를 받아오면 구분자로 파싱해서 보여주는 함수*/
+void parse(char* str){
+	char delimeter[10] = ",\n";
+	char *pch;
+
+	pch = strtok(str, delimeter);
+	while(pch){
+		printf("%s\n", pch);
+		pch = strtok(NULL, delimeter);
+	}
+
 }
